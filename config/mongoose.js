a76dbe229539deddfc7971e52fc6993a6ffa4293
Sandeep96Shah <82407422+Sandeep96Shah@users.noreply.github.com>
2021-09-08 00:40:07 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,8 @@
-//adding the mongoose library
+//mongoDb is used as a database
 const mongoose = require('mongoose');
 
-//setting up the connection
-mongoose.connect('mongodb://localhost/MERA-TODO-LIST');
-
-//acquiring the connection
-const db = mongoose.connection;
-
-//error
-db.on('error',console.error.bind(console,'error while connecting to database'));
-
-//successful
-db.once('open',function(){
-    console.log('successfully connected to the database');
-});
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true,})
+.then(() => {
+    console.log("Connected to the mongoose atlas!");
+})
+.catch((error)=>console.log("Cannot connect to the database!",error.message));
